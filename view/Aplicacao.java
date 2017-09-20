60350dc9f7e538716b684701b34cb6a42fb5ff26
carlosbarros <carlos.barros.22@gmail.com>
2017-09-20 11:41:12 -0300

    Classes usadas.
    Classes usadas.

---
+++ view/Aplicacao.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		
+		new Janela("Nova Janela");
+		
+		
+	
+	}
+}
---
+++ view/Janela.java
@@ -0,0 +1,69 @@
+package view;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Janela extends JFrame{
+	//Vamos construir uma interface com java swing.
+	//primeiro passo - declarar os componentes visuais
+	private JLabel lbNome;
+	private JTextField tfNome;
+	private JButton btSalvar;
+	//segundo passo - criar o método construtor da classe
+	public Janela(String titulo)
+	{
+		super(titulo);
+		//terceiro passo - instanciar cada componente visual
+		lbNome   = new JLabel("Digite seu nome");
+		tfNome   = new JTextField(20);
+		btSalvar = new JButton("Salvar");
+		
+		//quarto passo - definir o gerenciador de layout
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		//quinto passo - adicionar os componentes visuais ao container
+		add(lbNome);
+		add(tfNome);
+		add(btSalvar);
+		
+		//sexto passo - setar as configurações de janela
+		setVisible(true);
+		setSize(240, 240);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		//setimo passo - tratando eventos
+		//vamos implementar outra maneira de tratar
+		//o evento do botão - criando uma classe
+		//tratadora de eventos.
+		
+		TrataEventos handler = new TrataEventos();
+		btSalvar.addActionListener(handler);
+		
+		
+		
+	}
+	public class TrataEventos implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getSource() == btSalvar)
+				JOptionPane.showMessageDialog(null, "Ola, "+tfNome.getText());
+			
+		}
+		
+	}
+	
+	
+	
+	
+
+}
